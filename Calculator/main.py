7ca1d4a277c0494233fdec306cc65b9077ab1b7d
Talluri Prudhvi Nandhan Reddy <138387605+Nandhan0906@users.noreply.github.com>
2026-03-31 11:21:23 +0530

    add, sub, mul, div, mod

---
+++ Calculator/main.py
@@ -0,0 +1,33 @@
+'''
+from logic import *
+
+print(add(100, 50))
+print(sub(100, 50))
+print(mul(100, 50))
+print(div(100, 50))
+print(mod(100, 50))
+'''
+from logic import add, sub, mul, div, mod
+
+print(add(100, 50))
+print(sub(100, 50))
+print(mul(100, 50))
+print(div(100, 50))
+print(mod(100, 50))
+
+'''
+import logic
+print(logic.add(100, 50))
+print(logic.sub(100, 50))
+print(logic.mul(100, 50))
+print(logic.div(100, 50))
+print(logic.mod(100, 50))
+
+import logic as lg
+print(lg.add(100, 50))
+print(lg.sub(100, 50))
+print(lg.mul(100, 50))
+print(lg.div(100, 50))
+print(lg.mod(100, 50))
+
+'''
